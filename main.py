346c80e0c6f94e268f292b06afcfd980228fdf9d
GuuGaDev <132029160+GuuGaDev@users.noreply.github.com>
2023-05-04 16:46:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from biblioteca_revisao import *
+ 
 
+print(adicao(10,10))
+
+print(subtracao(10,10))
+
+print(divisao(10,10))
+
+print(divisao_inteiro(11,18))
+
+print(multiplicacao(10,10))
+
+print('Esta foi a atividade de hoje ! ')
