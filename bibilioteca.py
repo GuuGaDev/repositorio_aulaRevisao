169da893bec75d3346ec44431d6f4069484d63a2
GuuGaDev <132029160+GuuGaDev@users.noreply.github.com>
2023-05-04 16:46:12 -0300

    Update bibilioteca.py

--- bibilioteca.py
+++ bibilioteca.py
@@ -1,1 +1,17 @@
+def adicao(n1, n2):
+    return n1 + n2 
 
+def subtracao(n1,n2):
+    return n1 - n2
+
+def divisao(n1,n2):
+    return n1 / n2
+
+def multiplicacao(n1,n2):
+    return n1 * n2
+
+def divisao_inteiro(n1,n2):
+    return n1 // n2
+
+def resto_divisao(n1,n2):
+    return n1 % n2
